037da105029690a9d14e60a2ff34a3bf99a8f101
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-02-13 23:01:06 -0500

    submitting homework

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# 03-Password-Generator
+# Password Generator Project
+
+## Description
+
+This project we used JavaScript to generate a random password from the criteria that the user inputs.
+How it works is prompts will appear on the screen for users to answer and then after all of the prompts are answered a randomized password will be created for the user to copy and paste.
+
+What I did first was created all the prompts for the user to answer.
+I then created "if" statements. 
+
+finally all the heavy lifting is being done with the for loop at the botton.
+
+This was especially challenging to complete and have it generate a random password. I learned a lot about how 
+we need to provide logic to the code to make it function properly. I will go back and tweak it more to get the results that I want it to have. 
+
+
+## Usage
+
+GitHub Repo:
+
+
+Deployed Site:
+
+
+Screenshot:
+
+
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+w3schools
+https://www.w3schools.com/js/js_loop_for.asp
+
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Christianmsm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,12 +1,87 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+let lowerCaseEl = ["a","b","c","d,","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+let upperCaseEl = ["A","B","C","D,","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"];
+let specialCharacterEl = ["!","@","#","$","%","^","&","*","(",",",".",">",",","<"];
+let numberEl = ["1","2","3","4","5","6","7","8","9","0"];
+let passwordArray = [""];
+// var isLowerCase = document.querySelector("#lowercase");
+// var isUpperCase = document.querySelector("#uppercase");
+// var isNumber = document.querySelector("#numbers");
+// var isSpecialCharacter = document.querySelector("#specialCharacter");
+// var form = document.querySelector("#form");
 
+function theLength() {
+  let passwordLength = prompt("How many characters long would you like your password to be?");
+if (passwordLength >= 8 && passwordLength <= 128); {
+  return passwordLength;
+}
+}
+
+function lowerCaseOption() {
+  let randomLowerCase = lowerCaseEl[Math.floor(Math.random() * lowerCaseEl.length)];
+  let lowerCaseChoice = lowerCaseEl(randomLowerCase);
+  return lowerCaseChoice;
+}
+
+function upperCaseOption() {
+  let randomUpperCase = Math.floor(Math.random() * upperCaseEl.length);
+  let upperCaseChoice = upperCaseEl(randomUpperCase);
+  return upperCaseChoice;
+}
+
+function specialCharacterOption() {
+  let randomSpecialCharacter = Math.floor(Math.random() * specialCharacterEl.length);
+  let specialCharacterCaseChoice = specialCharacterEl(randomSpecialCharacter);
+  return specialCharacterCaseChoice;
+}
+
+function numberOption() {
+  let randomNumber = Math.floor(Math.random() * numberEl.length);
+  let numberChoice = numberEl(randomNumber);
+  return numberChoice;
+}
+
+function generatePassword() {
+passwordArray.length = 0
+
+let passwordLength = prompt("How many characters long would you like your password to be?");
+if (passwordLength >= 8 && passwordLength <= 128); {
+}
+
+let passwordLowerCase = prompt("would you like to include a lowercase? Y/N?");
+if (passwordLowerCase === "Y"); {
+passwordArray.push(lowerCaseEl);
+}
+
+
+let passwordUpperCase = prompt("Would you like to include an uppercase? Y/N?")
+if (passwordUpperCase === "Y"); {
+passwordArray.push(upperCaseEl);
+}
+
+let passwordNumeric = prompt("Would you like to include a numeric? Y/N?");
+if (passwordNumeric === "Y"); {
+  passwordArray.push(numberEl);
+}
+
+let passwordSpecChar = prompt("Would you like to include a special character? Y/N?");
+if (passwordSpecChar === "Y");{
+  passwordArray.push(specialCharacterEl);
+}
+let result = "";
+ for (let i = 0; i < passwordLength; i++) {
+passwordOption = Math.floor(Math.random() * passwordArray.length); 
+chosenCharacter = passwordArray[passwordOption];
+result += chosenCharacter;
+console.log(result);
+return result;
+ }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -15,3 +90,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// 
